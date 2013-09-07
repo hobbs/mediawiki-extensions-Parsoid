3ef1c7816b36b6cba04c2eaf0e2690c8ef85bfab
Inez Korczynski <inez@wikia-inc.com>
2013-09-07 06:19:27 +0000

    Proper fix for img_class (tests are still passing and it fallbacks nicely in case of old MW api)

--- js/lib/mediawiki.WikiConfig.js
+++ js/lib/mediawiki.WikiConfig.js
@@ -396,7 +396,7 @@ WikiConfig.prototype.getMagicPatternMatcher = function ( optionsList ) {
 		if ( ix > 0 ) {
 			regexString += '|';
 		}
-		aliases = this._interpolatedMagicWordAliases[optionsList[ix]];
+		aliases = this._interpolatedMagicWordAliases[optionsList[ix]] || [];
 		regexString += aliases.join( '|' )
 			.replace( /((?:^|\|)(?:[^\$]|\$[^1])*)($|\|)/g, '$1$$1$2' )
 			.replace( /\$1/g, '(.*)' );
